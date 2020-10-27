387c687dfc29b8f20f4d1a6bb8fc5b9a53cfcca7
IsGoose <47721300+IsGoose@users.noreply.github.com>
2020-10-27 02:58:20 +0000

    Basic RCONClient Object

---
+++ src/RCON.Net/RCONClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RCON.Net
+{
+    public class RCONClient
+    {
+        private IPEndPoint _endpoint;
+        private Socket _socket;
+
+        private List<Packet> ReceivedPackets = new List<Packet>();
+        private List<Packet> SentPackets = new List<Packet>();
+
+        public RCONClient(string host = "127.0.0.1",int port = 2303,string password = "")
+            : this(new IPEndPoint(IPAddress.Parse(host),port),password)
+        {
+
+        }
+
+    }
+}
