2ba23e9cb9033856315d86d28f31461856480787
IsGoose <47721300+IsGoose@users.noreply.github.com>
2020-10-28 02:58:01 +0000

    onPacketReceived Event Code + TODOs
    Fires when Socket receives data.
    Parses Relevant Information

--- src/RCON.Net/RCONClient.cs
+++ src/RCON.Net/RCONClient.cs
@@ -71,6 +71,33 @@ namespace RCON.Net
                 p.PacketId = ++_packetId;
             await _socket.SendAsync(new ArraySegment<byte>(p.Assemble()), SocketFlags.None);
         }
+
+
+
+        private void onRCONPacketReceived(object sender, SocketAsyncEventArgs e)
+        {
+            var bytesReceived = e.Buffer;
+            Array.Resize(ref bytesReceived, e.BytesTransferred);
+            var packet = new Packet(++_packetId, bytesReceived);
+            if(packet.CompareChecksums())
+            {
+
+                //Acknowledge Packet Immediately if Checksums Match so we do not receive the same packet again (See TODO Below)
+                //TODO: Handle Multi-Packet Messages
+                Task.Run(async () => await SendPacketAsync(new Packet(_packetId, PacketType.ServerMessage, packet.SequenceNumber)));
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("New Packet Received");
+                Console.ResetColor();
+                Console.WriteLine(Helpers.Bytes2String(packet.RelevantPayloadBytes));
+
+                //TODO: Fire EventHandlers Dependent on PacketType
+                //TODO: Allow Users to assign custom event handlers for their needs
+
+            } else
+                Console.WriteLine($"Checksum Missmatch - Received: {Helpers.Bytes2String(packet.ReceivedChecksum)} Calculated: {Helpers.Bytes2String(packet.ReceivedChecksum)}");
+            _socket.ReceiveAsync(e);
+            
+
         }
     }
 }
