114a193ef0be7563ce58343193361663204a9d6b
IsGoose <47721300+IsGoose@users.noreply.github.com>
2020-10-28 02:55:10 +0000

    SendPacketAsync Method

--- src/RCON.Net/RCONClient.cs
+++ src/RCON.Net/RCONClient.cs
@@ -16,19 +16,29 @@ namespace RCON.Net
         private List<Packet> ReceivedPackets = new List<Packet>();
         private List<Packet> SentPackets = new List<Packet>();
 
-        public RCONClient(string host = "127.0.0.1",int port = 2303,string password = "")
-            : this(new IPEndPoint(IPAddress.Parse(host),port),password)
+        private const int MaxPacketSize = 2048;
+
+        private int _packetId = 0;
+
+        public RCONClient(string host = "127.0.0.1",int port = 2303)
+            : this(new IPEndPoint(IPAddress.Parse(host),port))
         {
 
         }
 
-        public RCONClient(IPEndPoint endpoint,string password) 
+        public RCONClient(IPEndPoint endpoint) 
         {
             _endpoint = endpoint;
             _socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
             _socket.Connect(endpoint);
             var loginPacket = new Packet(0, PacketType.Login, 0, BattlEyeCommand.None, password);
             _socket.Send(loginPacket.Assemble());
+        public async Task SendPacketAsync(Packet p)
+        {
+            if (p.PacketId == null)
+                p.PacketId = ++_packetId;
+            await _socket.SendAsync(new ArraySegment<byte>(p.Assemble()), SocketFlags.None);
+        }
         }
     }
 }
